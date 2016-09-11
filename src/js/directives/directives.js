78a35fa153da513afaecc8e046f29b14ef4ba9d4
jbabaei <babaei2006@gmail.com>
2016-09-11 18:12:38 +0430

    File Extension Filter

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -1,23 +1,39 @@
-(function(angular) {
+(function (angular) {
     'use strict';
     var app = angular.module('FileManagerApp');
 
-    app.directive('angularFilemanager', ['$parse', 'fileManagerConfig', function($parse, fileManagerConfig) {
+    app.directive('angularFilemanager', ['$parse', 'fileManagerConfig', function ($parse, fileManagerConfig) {
         return {
             restrict: 'EA',
             templateUrl: fileManagerConfig.tplPath + '/main.html'
         };
     }]);
 
-    app.directive('ngFile', ['$parse', function($parse) {
+    app.directive("fileExtension", function ($compile, fileManagerConfig) {
+        return {
+            restrict: "A",
+            terminal: true,
+            priority: 1000,
+            link: function link(scope, element, attrs) {
+                fileManagerConfig.fileExtension = element.attr("file-extension");
+
+                element.removeAttr("file-extension");
+                element.removeAttr("data-file-extension");
+
+                $compile(element)(scope);
+            }
+        };
+    });
+
+    app.directive('ngFile', ['$parse', function ($parse) {
         return {
             restrict: 'A',
-            link: function(scope, element, attrs) {
+            link: function (scope, element, attrs) {
                 var model = $parse(attrs.ngFile);
                 var modelSetter = model.assign;
 
-                element.bind('change', function() {
-                    scope.$apply(function() {
+                element.bind('change', function () {
+                    scope.$apply(function () {
                         modelSetter(scope, element[0].files);
                     });
                 });
@@ -25,16 +41,16 @@
         };
     }]);
 
-    app.directive('ngRightClick', ['$parse', function($parse) {
-        return function(scope, element, attrs) {
+    app.directive('ngRightClick', ['$parse', function ($parse) {
+        return function (scope, element, attrs) {
             var fn = $parse(attrs.ngRightClick);
-            element.bind('contextmenu', function(event) {
-                scope.$apply(function() {
+            element.bind('contextmenu', function (event) {
+                scope.$apply(function () {
                     event.preventDefault();
-                    fn(scope, {$event: event});
+                    fn(scope, { $event: event });
                 });
             });
         };
     }]);
-    
+
 })(angular);
